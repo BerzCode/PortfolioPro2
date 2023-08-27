88981aa280fd864a0ab76d95c565efa1e7a21b50
Saurabh Kirve <>
2023-08-27 14:26:39 +0530

    Added dark mode toggle functionality and styles

--- script.js
+++ script.js
@@ -4,3 +4,36 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggleButton = document.getElementById("theme-toggle-button");
+  const toggleInput = document.getElementById("toggle");
+
+  // Retrieve user preference from localStorage
+  const storedThemePreference = localStorage.getItem("darkMode");
+
+  // Apply initial theme based on user preference
+  const isDarkModePreferred = storedThemePreference === "true";
+  applyTheme(isDarkModePreferred);
+  toggleInput.checked = isDarkModePreferred;
+
+  // Toggle theme on button click
+  themeToggleButton.addEventListener("click", () => {
+    const isDarkMode = toggleInput.checked;
+    applyTheme(isDarkMode);
+
+    // Store user preference in localStorage
+    localStorage.setItem("darkMode", isDarkMode);
+  });
+
+  function applyTheme(isDarkMode) {
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+      document.body.classList.remove("light-mode");
+    } else {
+      document.body.classList.add("light-mode");
+      document.body.classList.remove("dark-mode");
+    }
+  }
+});
+
+
